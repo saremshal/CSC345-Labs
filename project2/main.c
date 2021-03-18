df1cd9310e81be2db86a0d24f15d3ba85a413459
Linh Ngo <Linh.ngo9876@gmail.com>
2021-03-18 16:19:08 -0400

    bug fix

--- project2/main.c
+++ project2/main.c
@@ -66,7 +66,7 @@ uint8_t check_column(void)
     return 1;
 }
 
-void check_square(row_position, column_position)
+uint8_t check_square(row_position, column_position)
 {
     int square[9] = {0};
     uint8_t is_value_present;
@@ -75,31 +75,19 @@ void check_square(row_position, column_position)
     {
         for( int n = column_position; n < column_position + 3; ++n)
         {
-          int square_num = board[m][n];
-          for(int p = 0; p < 9; ++p)
-          {
-            square[p] = square_num;
-          }
-        }
-    }
-
-
-
-    /*
-
-    for(int i=0;i<9;i++)
-    {
-        for(int k=1;k<10;k++)
-        {
-            is_value_present = 0;
-            for(int j=0;j<9;j++)
+            int square_num = board[m][n];
+            if(square[square_num-1]==0)
             {
-
+                is_value_present = 1;
+            }
+            else
+            {
+                return 0;
             }
         }
     }
-*/
 
+    return 1;
 }
 
 
@@ -129,6 +117,7 @@ int main(int argc, char** argv)
     char str[19];
     int row_position, column_position;
 
+
     int mode = atoi(argv[1]);
 
     printf("BOARD STATE IN input.txt:\n");
@@ -143,6 +132,8 @@ int main(int argc, char** argv)
             board[i][j] = atoi(&str[(j*2)]);
         }
     }
+
+        check_square(0,0);
 
     /*
     printf("\n");
