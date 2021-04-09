cdee3ad7b932ea80f9b625f65cf41fa9cb3f0947
Linh Ngo <Linh.ngo9876@gmail.com>
2021-04-09 18:34:41 -0400

    adding report

--- project3/main_pr.c
+++ project3/main_pr.c
@@ -3,10 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 
-#define PAGE_TABLE_SIZE (128)
+#define PAGE_TABLE_SIZE (256)
 #define TLB_SIZE (16)
 #define OFFSET_MASK (0x00FF)
-#define FRAME_SIZE (256)
+#define FRAME_SIZE (128)
 
 /* Structure For all the TLB Data */
 typedef struct
